ac98704f382ba80fb10b9ee2bf74dbd9d0aaac16
Simone Venturi <sventur@S1066713ca.local>
2022-02-28 16:24:12 -0800

    Corrected bug in branch_to_trunk mapping (system_of_components.py)

--- build/lib/romnet/data/__init__.py
+++
@@ -1,7 +0,0 @@
-from .blackbox import BlackBox
-from .pde      import PDE
-
-__all__ = [
-    "BlackBox",
-    "PDE",
-]
--- build/lib/romnet/pinn/__init__.py
+++
@@ -1,7 +0,0 @@
-from .gradient import order_0, order_n
-from .         import system 
-
-__all__ = [
-    "order_0",
-    "order_n",
-]
